818cc2ec37a2cc86fe95f4aefde77d72f75849db
khyltarras-art <khyltarras@gmail.com>
2025-12-22 01:23:05 +0800

    experimental

--- src/DragCards.js
+++ src/DragCards.js
@@ -5,8 +5,8 @@ import { twMerge } from "tailwind-merge";
 export const DragCards = () => {
   return (
     <section className="relative grid min-h-screen w-full place-content-center overflow-hidden bg-neutral-950">
-      <h2 className="relative z-0 text-[15vw] font-black text-neutral-800 md:text-[200px]">
-        PROJECTS<span className="text-indigo-500">.</span>
+      <h2 className="relative z-0 text-[20vw] font-black text-neutral-800 md:text-[200px]">
+        ASTRO<span className="text-indigo-500">.</span>
       </h2>
       <Cards />
     </section>
@@ -18,61 +18,55 @@ const Cards = () => {
 
   return (
     <div className="absolute inset-0 z-10" ref={containerRef}>
-      {/* PHOTO 1 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1635373670332-43ea883bb081?q=80&w=2781&auto=format&fit=crop"
-        alt="Project 1"
+        src="https://images.unsplash.com/photo-1635373670332-43ea883bb081?q=80&w=2781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="6deg"
         top="20%"
         left="25%"
         className="w-36 md:w-56"
       />
-      {/* PHOTO 2 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=2787&auto=format&fit=crop"
-        alt="Project 2"
+        src="https://images.unsplash.com/photo-1576174464184-fb78fe882bfd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="12deg"
         top="45%"
         left="60%"
         className="w-24 md:w-48"
       />
-      {/* PHOTO 3 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?q=80&w=2670&auto=format&fit=crop"
-        alt="Project 3"
+        src="https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="-6deg"
         top="20%"
         left="40%"
         className="w-52 md:w-80"
       />
-      {/* PHOTO 4 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?q=80&w=2609&auto=format&fit=crop"
-        alt="Project 4"
+        src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?q=80&w=2609&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="8deg"
         top="50%"
         left="40%"
         className="w-48 md:w-72"
       />
-      {/* PHOTO 5 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1602212096437-d0af1ce0553e?q=80&w=2671&auto=format&fit=crop"
-        alt="Project 5"
+        src="https://images.unsplash.com/photo-1602212096437-d0af1ce0553e?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="18deg"
         top="20%"
         left="65%"
         className="w-40 md:w-64"
       />
-      {/* PHOTO 6 */}
       <Card
         containerRef={containerRef}
-        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop"
-        alt="Project 6"
+        src="https://images.unsplash.com/photo-1622313762347-3c09fe5f2719?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Example image"
         rotate="-3deg"
         top="35%"
         left="55%"
@@ -113,13 +107,15 @@ const Card = ({ containerRef, src, alt, top, left, rotate, className }) => {
         zIndex,
       }}
       className={twMerge(
-        "drag-elements absolute w-48 bg-neutral-200 p-1 pb-4 cursor-grab active:cursor-grabbing",
+        "drag-elements absolute w-48 bg-neutral-200 p-1 pb-4",
         className
       )}
       src={src}
       alt={alt}
       drag
       dragConstraints={containerRef}
+      // Uncomment below and remove dragElastic to remove movement after release
+      //   dragMomentum={false}
       dragElastic={0.65}
     />
   );
