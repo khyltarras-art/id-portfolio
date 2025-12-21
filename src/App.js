55d1e645d6c37db43d1c9777d5a75b57c22e7143
khyltarras-art <khyltarras@gmail.com>
2025-12-21 23:44:50 +0800

    move the ID positioning

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
 
   return (
     <>
-      <group position={[0, 4, 0]}>
+      <group position={[3, 4, 0]}>
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
           <BallCollider args={[0.1]} />
